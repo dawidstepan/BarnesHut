836ca38e653acb53e070b5cfbe0998b0e6f31ae6
Max Spannring <e12104980@tuwien.ac.at>
2023-05-17 23:31:47 +0200

    fixed overloading & types in Vector2D.hpp

--- include/Vector2D.hpp
+++ include/Vector2D.hpp
@@ -5,20 +5,23 @@
  * \brief Simple data type, meant to store information about pos, vel & acc for the Body object
  *
  * can be initialized via initializer list
+ * implemented as header-only library
  */
 
 class Vector2D{
 public:
-    Vector2D(double x=0, double y=0) : x(x), y(y) {
+    Vector2D(long double x=0, long double y=0) : x(x), y(y) {
     }
 
     //overloading
     Vector2D operator/(int m){
-        x = x/m;
-        y = y/m;
+        Vector2D res;
+        res.x = x/m;
+        res.y = y/m;
+        return res;
     }
-    double x;
-    double y;
+    long double x;
+    long double y;
 };
 
 #endif //GROUPMPROJECT_VECTOR2D_HPP
